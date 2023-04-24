bbf9f5689f034fad5b5501d2701357a95efa7352
JSlaig <jsaligz@gmail.com>
2023-04-25 01:56:53 +0200

    UI idea starting to take shape

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-        # TODO: Find an use for bottom bar and button, logging?
+        # TODO: Bottom bar will be use to print on it the pictures of the process
         # create main entry and button
         self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
