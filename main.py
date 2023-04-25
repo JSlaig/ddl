0967134a849d26bbd18aeac98ac84fd86ba30ed2
JSlaig <jsaligz@gmail.com>
2023-04-25 02:13:55 +0200

    UI idea starting to take shape

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class App(customtkinter.CTk):
         super().__init__()
 
         # configure window
-        self.title("DDL.py")
+        self.title("DDL")
         # self.wm_iconbitmap('Assets/DDL.ico')  # Setting an icon for the application
 
         window_width, window_height = configure_geometry()
@@ -46,54 +46,62 @@ class App(customtkinter.CTk):
                                                  font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-        self.button_load = customtkinter.CTkButton(self.sidebar_frame, text="Load", command=self.sidebar_button_event)
-        self.button_load.grid(row=1, column=0, padx=20, pady=10)
+        self.load_button = customtkinter.CTkButton(self.sidebar_frame, text="Load", command=self.sidebar_button_event)
+        self.load_button.grid(row=1, column=0, padx=20, pady=10)
 
-        self.button_camera = customtkinter.CTkButton(self.sidebar_frame, text="Camera", command=self.sidebar_button_event)
-        self.button_camera.grid(row=2, column=0, padx=20, pady=10)
+        self.camera_button = customtkinter.CTkButton(self.sidebar_frame, text="Camera", command=self.sidebar_button_event)
+        self.camera_button.grid(row=2, column=0, padx=20, pady=10)
 
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+        self.appearance_mode_option_menu = customtkinter.CTkOptionMenu(self.sidebar_frame,
                                                                        values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_option_menu.grid(row=6, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
         self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+        self.scaling_option_menu = customtkinter.CTkOptionMenu(self.sidebar_frame,
                                                                values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+        self.scaling_option_menu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-        # TODO: Bottom bar will be use to print on it the pictures of the process
-        # create main entry and button
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-        # TODO: Set dev mode flag on function here
-        self.switch = customtkinter.CTkSwitch(master=self, text=f"Developer Mode")
-        self.switch.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        # TODO: Add some sliders in this frame when neccesary
+        # TODO: Add in this frame when neccesary
         # Frame for future sliders
         self.slider_frame = customtkinter.CTkFrame(self)
         self.slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+
+        # TODO: Modify in order to be able to swap through stages?
+        self.stage_buttons = customtkinter.CTkSegmentedButton(self.slider_frame)
+        self.stage_buttons.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
 
         # TODO: Add and remove these when necessary
         self.slider_1 = customtkinter.CTkSlider(self.slider_frame, from_=0, to=255, number_of_steps=255)
         self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
 
-        # TODO: Modify in order to be able to swap through stages?
-        self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_frame)
-        self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+
+
+        # TODO: Bottom bar will be use to print on it the pictures of the process
+        # create main entry and button
+        self.developer_logs_button = customtkinter.CTkButton(self, text="Developer Logs",
+                                                             command=self.open_dev_image_dialog_event)
+        self.developer_logs_button.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+
+        # TODO: Set dev mode flag on function here
+        self.switch = customtkinter.CTkSwitch(master=self, text=f"Developer Mode")
+        self.switch.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         # set default values
-        self.appearance_mode_optionemenu.set("Dark")
-        self.scaling_optionemenu.set("100%")
-        self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        self.seg_button_1.set("Value 2")
+        self.appearance_mode_option_menu.set("Dark")
+        self.scaling_option_menu.set("100%")
 
-    def open_input_dialog_event(self):
+        # TODO: Config will depend on whether we are on camera or load image version
+        self.stage_buttons.configure(values=["Image Adjustment", "Image Crop Preview", "Paragraph Adjustment"])
+        self.stage_buttons.set("Image Adjusting")
+
+    # TODO: Make this function show the developer stage pictures
+    def open_dev_image_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
         print("CTkInputDialog:", dialog.get_input())
 
